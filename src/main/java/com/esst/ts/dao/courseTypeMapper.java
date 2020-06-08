6752a788a1587404a9218e881f2b9d0b25492f24
shihy <13810855530@163.com>
2020-06-08 18:19:28 +0800

    删除无效数据库实体

--- src/main/java/com/esst/ts/dao/courseTypeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.esst.ts.dao;
-
-import com.esst.ts.model.courseType;
-
-public interface courseTypeMapper {
-    int insert(courseType record);
-
-    int insertSelective(courseType record);
-}
