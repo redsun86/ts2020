53b59d61b46faec6d95e97e7c2409f6022e3f40d
shihy <13810855530@163.com>
2020-06-16 13:20:27 +0800

    添加userLoginLog 用戶登錄日志

---
+++ src/main/java/com/esst/ts/model/userLoginLog.java
@@ -0,0 +1,43 @@
+package com.esst.ts.model;
+
+public class userLoginLog {
+    private Integer id;
+
+    private Integer userId;
+
+    private Long createTime;
+
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
