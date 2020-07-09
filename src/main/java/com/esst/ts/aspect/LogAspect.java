a12033209bf6ade9ce0f64ebec7a1c6527b683de
shihy <13810855530@163.com>
2020-07-09 11:21:09 +0800

    Revert "减少服务器无用log输出"
    This reverts commit 0000afc3dfe72a07cf1430cdeed2e4ef349dcf8b.

--- src/main/java/com/esst/ts/aspect/LogAspect.java
+++ src/main/java/com/esst/ts/aspect/LogAspect.java
@@ -16,27 +16,27 @@ import java.util.List;
 @Slf4j
 public class LogAspect {
 
-//    @Around(value = "execution(* com.esst.ts.controller.*.*(..))")
-//    public Object showLog(ProceedingJoinPoint point) {
-//        long start = System.currentTimeMillis();
-//        Object target = point.getTarget();
-//        Object[] args = point.getArgs();
-//        MethodSignature signature = (MethodSignature) point.getSignature();
-//        String name = signature.getName();
-//        Object result = null;
-//        try {
-//            try {
-//                result = point.proceed(args);
-//            }catch (Exception e) {
-//                log.info("{}-{} 异常信息：{}",target,name,e);
-//            } finally {
-//            }
-//        } catch (Throwable e) {
-//            e.printStackTrace();
-//        }
-//        long end = System.currentTimeMillis();
-//        log.info("{}-{} 返回结果：{}",target,name,JSON.toJSONString(result));
-//        log.info("{}-{} 请求时长：{}",target,name,end-start);
-//        return result;
-//    }
+    @Around(value = "execution(* com.esst.ts.controller.*.*(..))")
+    public Object showLog(ProceedingJoinPoint point) {
+        long start = System.currentTimeMillis();
+        Object target = point.getTarget();
+        Object[] args = point.getArgs();
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        String name = signature.getName();
+        Object result = null;
+        try {
+            try {
+                result = point.proceed(args);
+            }catch (Exception e) {
+                log.info("{}-{} 异常信息：{}",target,name,e);
+            } finally {
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        log.info("{}-{} 返回结果：{}",target,name,JSON.toJSONString(result));
+        log.info("{}-{} 请求时长：{}",target,name,end-start);
+        return result;
+    }
 }
