613bb2bc30c30b74b8b820552dd722dc673ade25
shihy <13810855530@163.com>
2020-07-23 16:45:28 +0800

    添加拦截器，对所有请求进行校验
    1 参数是否包含userId和token
    2 token是否有效
    登录（userLogin）接口除外

---
+++ src/main/java/com/esst/ts/interceptor/LoginInterceptor.java
@@ -0,0 +1,78 @@
+package com.esst.ts.interceptor;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.esst.ts.model.Result;
+import com.esst.ts.service.UserTokenService;
+import com.esst.ts.utils.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Resource
+    private UserTokenService userTokenService;
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        Result r = new Result();
+        httpServletResponse.reset();
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        PrintWriter pw = httpServletResponse.getWriter();
+
+        String userId = null, token = null;
+        Map map = httpServletRequest.getParameterMap();
+        Set keSet = map.entrySet();
+        for (Iterator itr = keSet.iterator(); itr.hasNext(); ) {
+            Map.Entry me = (Map.Entry) itr.next();
+            Object ok = me.getKey();
+            Object ov = me.getValue();
+            String[] value = new String[1];
+            if (ov instanceof String[]) {
+                value = (String[]) ov;
+            } else {
+                value[0] = ov.toString();
+            }
+            if (ok.toString().equals("token")) {
+                token = value[0];
+            }
+            if (ok.toString().equals("userId")) {
+                userId = value[0];
+            }
+        }
+
+        if (StringUtils.isEmpty(token) || StringUtils.isEmpty(userId)) {
+            r.setMsg("请求无效，缺少必要参数");
+            r.setCode(Result.PARAMETER_ERROR);
+            pw.write(JSONObject.toJSONString(r));
+            return false;
+        }
+
+        if (userTokenService.checkToken(Integer.parseInt(userId), token, 1)) {
+            r.setMsg("无效token");
+            r.setCode(Result.TOKEN_ERROR);
+            pw.write(JSONObject.toJSONString(r));
+            return false;
+        }
+        pw.flush();
+        pw.close();
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
